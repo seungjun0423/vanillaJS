e77e0e4d0d668791c4ef48191a31d1da22c22aa4
이승준 <87581422+seungjun0423@users.noreply.github.com>
2023-10-18 18:22:11 +0900

    2. 값 편집 항목 html 구조 작성

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
-/** parameter => (x: x축 수치, y: y추 수치)  그래프 수치를 나열해주는 함수 */
+/** parameter => (xList: x축 수치, yList: y축 수치)  그래프 수치를 나열해주는 함수 (배열 타입 입력)*/
 const listingGraphData = ( xList, yList) => {
-	// const $x = document.createElement('div');
-	// $x.className = "xData";
-	// $x.innerText = 3;
-	// console.log($x);
-	// console.log(document.getElementsByClassName("zero"));
-	// document.getElementsByClassName("xDataList")[0].appendChild($x);
-	// console.log(document.getElementsByClassName("yDataList"));
-
 	xList.map( el => {
 		const $xDataList = 	document.getElementsByClassName("xDataList")[0]
 		const $x = document.createElement('div');
@@ -15,7 +7,16 @@ const listingGraphData = ( xList, yList) => {
 		$x.innerText = el;
 		$xDataList.appendChild($x);
 	})
+
+	yList.map( el => {
+		const $yDataList = 	document.getElementsByClassName("yDataList")[0]
+		const $y = document.createElement('div');
+		$y.className = "yData";
+		$y.innerText = el;
+		$yDataList.appendChild($y);
+	})
 };
 const xList = [0,1,2,3,4];
-const yList = [0,10,20,30,40,50,60,70,80,90];
+const yList = [10,20,30,40,50,60,70,80,90,100];
+
 listingGraphData(xList,yList);
