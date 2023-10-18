ad0094fd750393073e2c3c2b9487df878f9b6edc
이승준 <87581422+seungjun0423@users.noreply.github.com>
2023-10-18 19:49:32 +0900

    2. 값 편집 항목table 구현 자바스크립트 작성

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+
+const xList = [0,1,2,3,4];
+const yList = [10,20,30,40,50,60,70,80,90,100];
+
+const data = [
+	{ id: 0, value: 75},
+	{ id: 1, value: 20},
+	{ id: 2, value: 80},
+	{ id: 3, value: 100},
+	{ id: 4, value: 70},
+]
 /** parameter => (xList: x축 수치, yList: y축 수치)  그래프 수치를 나열해주는 함수 (배열 타입 입력)*/
 const listingGraphData = ( xList, yList) => {
 	xList.map( el => {
@@ -16,7 +27,49 @@ const listingGraphData = ( xList, yList) => {
 		$yDataList.appendChild($y);
 	})
 };
-const xList = [0,1,2,3,4];
-const yList = [10,20,30,40,50,60,70,80,90,100];
 
 listingGraphData(xList,yList);
+
+
+/** 2. 값 편집 항목 테이블 구현 및 삭제 함수 */
+const setTable = ( data ) => {
+
+	/** 삭제 기능 */
+	const clickEvent = (e) => {
+		const target = e.target.parentNode;
+		target.remove();
+	};
+
+	data.map( (el,i) => {
+		const $itemWrapper = document.createElement('div');
+		$itemWrapper.className = `itemWrapper ${i}`;
+
+		const $id = document.createElement('div');
+		$id.className = "id";
+		$id.innerText = `${el.id}`;
+
+		const $value = document.createElement('div');
+		$value.className = "value";
+		$value.innerText = `${el.value}`; 
+
+		const $deleteBtn = document.createElement('div');
+		$deleteBtn.className = "deleteBtn";
+		$deleteBtn.innerText = `삭제`; 
+
+		/** 삭제 이벤트 버튼에 추가 */
+		$deleteBtn.addEventListener('click',(e)=>{clickEvent(e)});
+
+		$itemWrapper.appendChild($id);
+		$itemWrapper.appendChild($value);
+		$itemWrapper.appendChild($deleteBtn);
+
+		/** 만들어진 요소 html 삽입 */
+		const $table = document.getElementsByClassName("table")[0];
+		$table.appendChild($itemWrapper);
+	})
+};
+
+setTable( data );
+
+
+
